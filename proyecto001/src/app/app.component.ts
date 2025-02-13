fd06c012acd784ea8c8c2b7a8bb6718177bda2ee
Azahara <mariaazahara.parrales-cuevas@iesruizgijon.com>
2025-02-13 09:32:06 +0100

    Añadidos los cambios que hicimos en clase el otro día (en component.html, component.ts y module.ts)

--- proyecto001/src/app/app.component.ts
+++ proyecto001/src/app/app.component.ts
@@ -1,19 +1,54 @@
 import { Component } from '@angular/core';
 
+//componente padre
+//anotaciones
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
+  selector: 'app-root', //nombre del componente
+  templateUrl: './app.component.html', //url del componente
   standalone: false,
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css' //hoja de estilos del componente
 })
 export class AppComponent {
   title = 'proyecto001';
 
-  nombre = 'Azahara';
-  edad = 30;
+  //variable booleana
+  nombre: string = 'Azahara';
+
+  //variable booleana
   fumador = false;
 
+  //variable numerica
+  edad = 30;
+
+  //array
+  notas: number[] = [8.1, 5.6, 3.2];
+  //notas: Array<number> = [8.1,5.6,3.2];
+
+  //contador, aumentar y decrementar
+  contador = 0;
+  incrementarContador() {
+    this.contador++;
+  }
+
+  decrementarContador() {
+    this.contador--;
+  }
+
+  //array de objetos para hacer un select
+  provincias = [
+    { id: 1, nombre: "Huelva" },
+    { id: 2, nombre: "Córdoba" },
+    { id: 3, nombre: "Jaén" },
+    { id: 4, nombre: "Cádiz" }
+  ]
+
+  //funcion booleana
   esFumador() {
-    return this.fumador ? "Sí es fumador" : "No es fumador";
+    return this.fumador ? "Sí fumo" : "No fumo";
+  }
+
+  //evento, pulsar boton
+  pulsarBoton() {
+    alert("Has pulsado el botón.");
   }
 }
--- proyecto001/src/app/app.module.ts
+++ proyecto001/src/app/app.module.ts
@@ -3,6 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+//hay cosas en angular que necesitaran X módulos, estos módulos habrá que importarlos aquí, como este de FormsModule (importado por mí)
+import { FormsModule } from '@angular/forms'; 
 
 @NgModule({
   declarations: [
@@ -10,7 +12,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule //aqui se añade el modulo
   ],
   providers: [],
   bootstrap: [AppComponent]
